4a8de0a672d9860c0493bd8f8eea65011ef89a6b
HemantKhiratkar <khiratkarhemant@gmail.com>
2021-06-19 14:07:35 +0530

    Delete Day5 directory

--- Day5/check for array.js
+++
@@ -1,9 +0,0 @@
-is_array = function(input) 
-    {
-    if (toString.call(input) === "[object Array]")
-      return true;
-    return false;   
-    };
-      console.log(is_array('w3resources'));
-      console.log(is_array([1,2,3,4]));
-      
--- Day5/join array.js
+++
@@ -1,3 +0,0 @@
-mycolor = ["red","green","white","black"];
-console.log(mycolor.toString());
-console.log(mycolor.join('+'));
