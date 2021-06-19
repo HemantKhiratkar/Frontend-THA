d65cc89d6544262207b74d17adbe5e6bc2b4c996
HemantKhiratkar <khiratkarhemant@gmail.com>
2021-06-19 14:08:25 +0530

    Add files via upload

---
+++ Day6/2.js
@@ -0,0 +1,6 @@
+var array_clone = function(input)
+{
+    return input.slice();
+};
+console.log(array_clone(['w','e','r','a']));
+console.log(array_clone([1, 2, [4, 0]]));
---
+++ Day6/4.js
@@ -0,0 +1,3 @@
+mycolor = ["red","green","white","black"];
+console.log(mycolor.toString());
+console.log(mycolor.join('+'));
