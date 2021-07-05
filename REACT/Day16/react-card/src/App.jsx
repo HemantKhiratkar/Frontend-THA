1295a2acb76a19e96ce906613bb9ed9a11921677
HemantKhiratkar <khiratkarhemant@gmail.com>
2021-07-05 17:34:57 +0530

    Add files via upload

---
+++ REACT/Day16/react-card/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/card";
+import Card2 from "./components/card2";
+import "./style.css";
+
+
+function App() {
+    return(
+        <div className = "App">
+            <Card></Card>
+            <Card2 />
+        </div>
+    );
+}
+
+
+export default App;
